0dbadc12d385207936253c9cea9adf06174dc35d
g-yunjh <122170238+g-yunjh@users.noreply.github.com>
2024-09-15 17:14:31 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        int hap = 0;
+        int gop = 1;
+        
+        for (int i = 0 ; i < num_list.length; i ++ ) {
+            hap += num_list[i];
+        }
+        
+        hap = hap*hap;
+        
+        for (int i = 0; i < num_list.length; i ++ ) {
+            gop = gop * num_list[i];
+        }
+        
+        if (gop < hap) {
+            answer = 1;
+        } else if (gop > hap){
+            answer = 0;
+        }
+        
+        
+        return answer;
+    }
+}
