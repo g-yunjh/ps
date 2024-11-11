19c5877498d5da3edee612ef81611d3cb3772628
g-yunjh <122170238+g-yunjh@users.noreply.github.com>
2024-11-11 14:40:24 +0900

    [Bronze I] Title: 평균, Time: 200 ms, Memory: 18664 KB -BaekjoonHub

--- Java/백준/Bronze/1546. 평균/평균.java
+++ Java/백준/Bronze/1546. 평균/평균.java
@@ -5,18 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int arr[] = new int[N];
-        for (int i = 0; i <N ; i++) {
-            arr[i] = sc.nextInt();
-        }
+        int A[] = new int[N];
 
+        for (int i = 0 ; i < N ; i++) {
+            A[i] = sc.nextInt();
+        }
         long sum = 0;
         long max = 0;
-        for (int i = 0; i<N ; i ++){
-            if (arr[i] > max) max = arr[i];
-            sum = sum + arr[i];
+        for (int i = 0; i < N; i++) {
+            if (A[i] > max) {
+                max = A[i];
+            }
+            sum += A[i];
         }
-
-        System.out.println(sum*100.0 / max / N);
-}
+        System.out.print(sum*100.0/max/N);
+    }
 }
