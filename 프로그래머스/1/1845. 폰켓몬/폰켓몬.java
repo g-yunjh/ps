d120338d4b949d960d46fa8917e5b5f3766834fc
Yun Juhwan <122170238+g-yunjh@users.noreply.github.com>
2025-12-16 18:25:44 +0900

    [level 1] Title: 폰켓몬, Time: 6.39 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,21 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> uniquePokemons = new HashSet<>();
-        for (int num : nums) {
-            uniquePokemons.add(num);
+        Arrays.sort(nums);
+        
+        int cnt = 0;
+        int tmp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != tmp) {
+                cnt += 1;
+                tmp = nums[i];
+            }
         }
-
-        int maxSelectable = nums.length / 2;
-
-        return Math.min(uniquePokemons.size(), maxSelectable);
+        if (cnt >= nums.length/2) {
+            return nums.length/2;
+        } else {
+            return cnt;
+        }
     }
 }
