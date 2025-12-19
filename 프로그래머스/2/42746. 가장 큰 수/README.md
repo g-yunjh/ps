473817dd9943e84bdfc8679197a76b54319c76b7
Yun Juhwan <122170238+g-yunjh@users.noreply.github.com>
2025-12-19 16:10:18 +0900

    [level 2] Title: 가장 큰 수, Time: 1632.07 ms, Memory: 23.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=python3) 
 
 ### 성능 요약
 
-메모리: 26.2 MB, 시간: 55.67 ms
+메모리: 23.1 MB, 시간: 1632.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 10일 16:16:19
+2025년 12월 19일 16:10:16
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,23 +1,21 @@
+from functools import cmp_to_key
+
 def solution(numbers):
-    # 1. 모든 숫자를 문자열로 변환 (정렬 비교를 위해)
-    str_numbers = list(map(str, numbers))
+    s = [str(n) for n in numbers]
     
-    # 2. 커스텀 정렬 (람다 함수 이용)
-    # 두 문자열 a와 b를 비교할 때, (a + b)와 (b + a)의 크기를 비교하여 정렬합니다.
-    # key=lambda x: x * 3 : 
-    #   파이썬에서는 key에 문자열을 전달하면, 문자열을 기준으로 비교합니다.
-    #   '3' * 3 = '333', '30' * 3 = '303030' 처럼 문자열을 반복하여
-    #   길이가 다른 숫자(예: 3과 30)를 공평하게 비교할 수 있게 합니다.
-    #   (numbers 원소의 최대값이 1000이므로 3자리까지 반복하면 충분합니다.)
+    # 2. 커스텀 비교 함수 정의 (x+y 와 y+x 중 큰 것을 선택)
+    def compare(x, y):
+        if x + y > y + x:
+            return -1  # x가 앞으로
+        elif x + y < y + x:
+            return 1   # y가 앞으로
+        else:
+            return 0
     
-    str_numbers.sort(key=lambda x: x * 3, reverse=True)
+    s.sort(key=cmp_to_key(compare))
     
-    # 3. 정렬된 문자열 리스트를 하나의 문자열로 합침
-    answer = "".join(str_numbers)
+    answer = ''.join(s)
+    answer = int(answer)
+    answer = str(answer)
     
-    # 4. 특수 예외 처리: [0, 0, 0]과 같이 모두 0인 경우
-    # 결과가 '000'이 아닌 '0'이 되도록 처리합니다.
-    if answer[0] == '0':
-        return '0'
-        
     return answer
