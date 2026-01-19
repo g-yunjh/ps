c9ab4b302594a506f918bfb257119af250ac7c78
Yun Juhwan <122170238+g-yunjh@users.noreply.github.com>
2026-01-19 17:55:15 +0900

    [level 2] Title: H-Index, Time: 177.07 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,18 +1,17 @@
 def solution(citations):
-    citations.sort()
-    
-    h = citations[-1]
+    citations.sort(reverse = True)
+    h = citations[0]
     while True:
-        up = []
-        under = []
-        
-        for i in citations:
-            if (i >= h):
-                up.append(i)
+        up = 0
+        for citation in citations:
+            if citation >= h:
+                up += 1
             else:
-                under.append(i)
-        
-        if (len(up) >= h) & (len(under) <= h):
+                break
+        if (up >= h) and (len(citations) - up <= h):
             return h
         else:
             h -= 1
+        
+            
+        
