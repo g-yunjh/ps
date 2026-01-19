4ce71c81e8a713bdedb9aab29883c117b8bada49
Yun Juhwan <122170238+g-yunjh@users.noreply.github.com>
2026-01-19 17:35:55 +0900

    [level 2] Title: 가장 큰 수, Time: 1286.02 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,21 +1,7 @@
-from functools import cmp_to_key
-
 def solution(numbers):
-    s = [str(n) for n in numbers]
+    arr = []
+    for number in numbers:
+        arr.append(str(number))
+    arr.sort(key = lambda x: x*3, reverse = True)
     
-    # 2. 커스텀 비교 함수 정의 (x+y 와 y+x 중 큰 것을 선택)
-    def compare(x, y):
-        if x + y > y + x:
-            return -1  # x가 앞으로
-        elif x + y < y + x:
-            return 1   # y가 앞으로
-        else:
-            return 0
-    
-    s.sort(key=cmp_to_key(compare))
-    
-    answer = ''.join(s)
-    answer = int(answer)
-    answer = str(answer)
-    
-    return answer
+    return str(int("".join(arr)))
