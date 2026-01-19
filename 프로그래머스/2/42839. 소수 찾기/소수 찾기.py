3f82b27918197ce138c3613bd31d12556ca48d1c
Yun Juhwan <122170238+g-yunjh@users.noreply.github.com>
2026-01-19 20:46:29 +0900

    [level 2] Title: 소수 찾기, Time: 7.62 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,27 +1,19 @@
-import itertools
+from itertools import permutations
 
 def is_prime(n):
-    if n < 2:
-        return False
+    if n < 2: return False
     for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
+        if n % i == 0: return False
     return True
 
 def solution(numbers):
-    num_set = set()
-    
-    # 1. 모든 숫자 조합 만들기 (1글자부터 n글자까지)
+    nums = set()
     for i in range(1, len(numbers) + 1):
-        # itertools.permutations는 튜플을 반환하므로 합쳐서 숫자로 변환
-        perms = itertools.permutations(list(numbers), i)
-        for p in perms:
-            num_set.add(int("".join(p)))
+        # 1글자부터 n글자까지 모든 순열 생성
+        for p in permutations(numbers, i):
+            nums.add(int(''.join(p)))
     
-    # 2. 생성된 숫자 중 소수의 개수 세기
-    answer = 0
-    for n in num_set:
-        if is_prime(n):
-            answer += 1
-            
-    return answer
+    count = 0
+    for n in nums:
+        if is_prime(n): count += 1
+    return count
