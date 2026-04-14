4f29a8725a0518e84b27cf478d753ae0628f5280
Yun Juhwan <122170238+g-yunjh@users.noreply.github.com>
2026-04-14 10:20:20 +0900

    [level 3] Title: 입국심사, Time: 532.66 ms, Memory: 13.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,19 @@
+def solution(n, times):
+    ans = 0
+    
+    left = 1
+    right = n * max(times)
+    
+    while left <= right:
+        mid = (left + right)//2
+        cnt = 0
+        
+        for time in times:
+            cnt += mid // time
+        
+        if cnt >= n:
+            ans = mid
+            right = mid -1
+        else:
+            left = mid + 1
+    return ans
