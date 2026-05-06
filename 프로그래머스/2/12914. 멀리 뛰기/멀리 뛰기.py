ae2577019e9ecf99b4077c96c6f058abb9d722c4
Yun Juhwan <122170238+g-yunjh@users.noreply.github.com>
2026-05-06 19:51:58 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.67 ms, Memory: 9.31 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    d = [0] * (n+1)
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    d[1] = 1
+    d[2] = 2
+    for i in range(3, n+1):
+        d[i] = d[i-1] + d[i-2]
+    return d[n] % 1234567
