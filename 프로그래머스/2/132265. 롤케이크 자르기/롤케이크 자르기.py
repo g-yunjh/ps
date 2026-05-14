83eaa806c1a4739e054f0dcfb16e3ea8f31077d0
Yun Juhwan <122170238+g-yunjh@users.noreply.github.com>
2026-05-14 16:24:31 +0900

    [level 2] Title: 롤케이크 자르기, Time: 386.70 ms, Memory: 50.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def solution(topping):
+    ans = 0
+    
+    left  = set()
+    right = Counter(topping)
+    
+    for t in topping:
+        left.add(t)
+        right[t] -= 1
+        if right[t] == 0:
+            del right[t]
+        if len(left) == len(right):
+            ans += 1
+    return ans
