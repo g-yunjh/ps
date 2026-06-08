f50d6d811167e0222efda01a0b312d367b1c86dc
g-yunjh <122170238+g-yunjh@users.noreply.github.com>
2026-06-08 10:35:25 +0900

    [level 2] Title: 올바른 괄호, Time: 5.19 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,15 @@
 def solution(s):
     st = []
+    s = list(s)
     for i in s:
-        if (i == "("):
+        if i == "(":
             st.append(1)
-        elif (not st):
+        elif st == []:
             return False
         else:
             st.pop()
-    if (st):
-        return False
-    else:
+    
+    if st == []:
         return True
-        
+    else:
+        return False
