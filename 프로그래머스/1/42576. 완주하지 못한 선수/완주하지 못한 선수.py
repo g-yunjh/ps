adaad03bdb00a66d9bc81d675ea3280cad0cabce
g-yunjh <122170238+g-yunjh@users.noreply.github.com>
2026-06-11 09:26:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.13 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,11 @@
-from collections import Counter
-
 def solution(participant, completion):
-    answer = Counter(participant) - Counter(completion)
-    return list(answer.keys())[0]
+    participant.sort()
+    completion.sort()
+    
+    # completion의 길이만큼 비교
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i] # 달라지는 순간 그 사람이 범인
+            
+    # 반복문이 다 끝났는데도 안 걸렸다면, 정렬 후 맨 마지막에 있는 사람이 범인
+    return participant[-1]
