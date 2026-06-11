d5d102eddd84a617f131a95069d28ac928c9454d
g-yunjh <122170238+g-yunjh@users.noreply.github.com>
2026-06-11 11:33:34 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,22 +1,32 @@
-from collections import defaultdict
-
 def solution(genres, plays):
-    answer = []
-    genre_total = defaultdict(int) # 장르별 총 재생 횟수
-    genre_songs = defaultdict(list) # 장르별 곡 정보 (재생수, 고유번호)
-    
+    arr = []
     for i in range(len(genres)):
-        genre_total[genres[i]] += plays[i]
-        genre_songs[genres[i]].append((plays[i], i))
-        
-    # 1. 많이 재생된 장르 순으로 정렬
-    sorted_genres = sorted(genre_total.items(), key=lambda x: x[1], reverse=True)
+        arr.append([genres[i], plays[i], i])
     
-    for genre, total in sorted_genres:
-        # 2. 장르 내에서 많이 재생된 순, 같으면 고유번호 낮은 순 정렬
-        songs = sorted(genre_songs[genre], key=lambda x: (-x[0], x[1]))
+    m = {}
+    for i in arr:
+        if i[0] in m:
+            m[i[0]] += i[1]
+        else:
+            m[i[0]] = i[1]
+    m = sorted(m.items(), key = lambda x: x[1], reverse = True)
+
+    t0 = []
+    for i in m:
+        tmp = []
+        for j in arr:
+            if j[0] == i[0]:
+                tmp.append(j[1:])
+        t0.append(tmp)
+    
+    ans = []
+    for i in t0:
+        i.sort(key = lambda x: (-x[0], x[1]))
+        cnt = 0
+        for j in i:
+            if cnt < 2:
+                ans.append(j[1])
+                cnt += 1
+    return ans
         
-        # 3. 장르별 최대 2곡까지 추가
-        answer.extend([idx for play, idx in songs[:2]])
-        
-    return answer
+            
