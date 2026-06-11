88150d081864c171eeb6199c7863c565a1a84f57
g-yunjh <122170238+g-yunjh@users.noreply.github.com>
2026-06-11 10:35:22 +0900

    [level 2] Title: 전화번호 목록, Time: 371.06 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,15 @@
 def solution(phone_book):
-    phone_book.sort()
-    for i in range(len(phone_book) - 1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            return False
+    # 1. 모든 전화번호를 해시 맵(딕셔너리)에 등록
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    
+    # 2. 각 번호의 접두어가 해시 맵에 존재하는지 확인
+    for phone_number in phone_book:
+        jumsu = ""
+        for number in phone_number:
+            jumsu += number
+            if jumsu in hash_map and jumsu != phone_number:
+                return False
+                
     return True
