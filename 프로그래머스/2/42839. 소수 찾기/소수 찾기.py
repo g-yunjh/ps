9ce44babb9e6480bddfee695c7b7818ed720d909
g-yunjh <122170238+g-yunjh@users.noreply.github.com>
2026-06-22 10:40:26 +0900

    [level 2] Title: 소수 찾기, Time: 3.97 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,19 +1,30 @@
 from itertools import permutations
+import math
 
 def is_prime(n):
-    if n < 2: return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0: return False
-    return True
+    # 1과 그 이하의 수는 소수가 아님
+    if n <= 1:
+        return False
+    
+    # 2부터 n의 제곱근까지의 숫자만 확인
+    for i in range(2, int(math.isqrt(n)) + 1):
+        if n % i == 0:
+            return False  # 나누어 떨어지면 소수가 아님
+            
+    return True  # 루프를 무사히 통과하면 소수
 
 def solution(numbers):
-    nums = set()
-    for i in range(1, len(numbers) + 1):
-        # 1글자부터 n글자까지 모든 순열 생성
-        for p in permutations(numbers, i):
-            nums.add(int(''.join(p)))
+    n = list(numbers)
     
-    count = 0
-    for n in nums:
-        if is_prime(n): count += 1
-    return count
+    arr0 = []
+    for i in range(1, len(n) + 1):
+        tmp = permutations(n,i)
+        for j in tmp:
+            arr0 .append(int("".join(j)))
+            
+    arr1 = []
+    for i in arr0:
+        if is_prime(i) == True:
+            arr1.append(i)
+    arr1 = set(arr1)
+    return len(arr1)
