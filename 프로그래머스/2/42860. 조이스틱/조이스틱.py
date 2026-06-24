a68e47c979efdf83a1a7a2020699830df2201e63
g-yunjh <122170238+g-yunjh@users.noreply.github.com>
2026-06-24 20:03:54 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,16 +1,19 @@
 def solution(name):
-    up_down = 0
+    up_down_moves = 0
     for char in name:
-        up_down += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
-    
+        up_down_moves += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        
     n = len(name)
-    left_right = n - 1
-    
+    min_left_right = n - 1  
     for i in range(n):
         next_idx = i + 1
         while next_idx < n and name[next_idx] == 'A':
             next_idx += 1
             
-        left_right = min(left_right, i * 2 + n - next_idx, (n - next_idx) * 2 + i)
+        path1 = min_left_right
+        path2 = i * 2 + (n - next_idx)
+        path3 = (n - next_idx) * 2 + i
         
-    return up_down + left_right
+        min_left_right = min(path1, path2, path3)
+        
+    return up_down_moves + min_left_right
